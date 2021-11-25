316961acfb09305cce953e0b13775fb49a1196f1
arlenptolentino <atolentino@grupoasesores.com.mx>
2021-11-25 09:47:52 -0600

    Initial Commit

---
+++ MyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyList
+//
+//  Created by Arlen Peña on 25/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
