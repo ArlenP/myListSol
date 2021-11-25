b6ec5b96ed3ce9e01851a2c54b8af6edf85569e6
arlenptolentino <atolentino@grupoasesores.com.mx>
2021-11-25 10:00:48 -0600

    Creacion de tabla para lista

--- MyList/ViewController.swift
+++ MyList/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var todo: [String] = []
+    @IBAction func btnAgregar(_ sender: Any) {
+        let alert = UIAlertController(title: "Nueva Tarea",
+                                        message: "Agregar un nuevo titulo",
+                                        preferredStyle: .alert)
+          let saveAction = UIAlertAction(title: "Guardar",
+                                         style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first,
+            let nameToSave = textField.text else {
+                    return
+              }
+              self.todo.append(nameToSave)
+              self.myList.reloadData()
+          }
+          let cancelAction = UIAlertAction(title: "Cancel",
+                                           style: .cancel)
+          alert.addTextField()
+          alert.addAction(saveAction)
+          alert.addAction(cancelAction)
+          present(alert, animated: true)
+    }
+    
+    
+    
+    @IBOutlet weak var myList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Mi lista"
+        myList.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
 
 }
 
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:indexPath)
+        cell.textLabel?.text = todo[indexPath.row]
+        return cell
+    }
+    
+    
+}
